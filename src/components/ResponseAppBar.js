7a39f8b3a3bd15c9a418b835f2b6c9b384138b04
Bryan Carl <bryancarl7@gmail.com>
2024-09-15 16:47:47 -0700

    added in theme

--- src/components/ResponseAppBar.js
+++ src/components/ResponseAppBar.js
@@ -7,14 +7,25 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
+import { createTheme } from '@mui/material/styles';
 
 const pages = ['Wedding', 'Hotels', 'Map'];
-const settings = ['Profile', 'Account', 'About Us', 'Settings', 'Logout'];
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#9FE2BF',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#9FE2BF',
+      contrastText: '#000',
+    },
+  },
+});
 
 const ResponsiveAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -40,7 +51,7 @@ const ResponsiveAppBar = () => {
   };
 
   return (
-    <AppBar position="static" color="#9FE2BF">
+    <AppBar position="static" theme={theme}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
